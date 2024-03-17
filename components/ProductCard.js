01eb6fd5053de2299d6596ce24c645593e9828f8
Lazarous Deredza <lazarousderedza99@gmail.com>
2024-03-17 16:50:49 +0200

    first

---
+++ components/ProductCard.js
@@ -0,0 +1,129 @@
+import { View, Text, StyleSheet, Image ,Platform} from "react-native";
+
+const getTypeDetails = (type) => {
+  switch (type.toLowerCase()) {
+  
+    case type.toLowerCase():
+      return { borderColor: "#FF5733", emoji: "$" };
+      
+   
+  }
+};
+
+export default function ProductCard({
+  name,
+  image,
+  type,
+  hp,
+  created_at,
+  updated_at,
+}) {
+  const { borderColor, emoji } = getTypeDetails(type);
+  return (
+    <View style={styles.card}>
+      <View style={styles.nameContainer}>
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.hp}>Weight: {hp}</Text>
+      </View>
+
+      <Image
+        source={image}
+        accessibilityLabel={`${name} Product`}
+        style={styles.image}
+        resizeMode="contain"
+      />
+
+      <View style={styles.typeContainer}>
+        <View style={[styles.badge, { borderColor }]}>
+          <Text style={styles.typeEmoji}>{emoji}</Text>
+          <Text style={styles.typeText}>{type}</Text>
+        </View>
+      </View>
+
+      <View style={styles.dateContainer}>
+        <Text style={styles.dateText}>Created At : {created_at}</Text>
+      </View>
+
+      <View style={styles.updateDateContainer}>
+        <Text style={styles.updateDateText}>
+          Updated At : {updated_at}
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+    borderRadius: 16,
+    borderWidth: 2,
+    padding: 16,
+    margin: 16,
+    ...Platform.select({
+      ios: {
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: "#333",
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+  nameContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    marginBottom: 32,
+  },
+  name: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  hp: {
+    fontSize: 22,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    marginBottom: 16,
+  },
+  typeContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 40,
+  },
+  badge: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 20,
+    borderWidth: 4,
+  },
+  typeEmoji: {
+    fontSize: 30,
+    marginRight: 12,
+  },
+  typeText: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  dateContainer: {
+    marginBottom: 12,
+  },
+  dateText: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  updateDateContainer: {
+    marginBottom: 8,
+  },
+  updateDateText: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+});
--- screens/Products.js
+++ screens/Products.js
@@ -1,47 +1,109 @@
-import {  useEffect, useState } from 'react';
-import { View, Text, FlatList,Button } from 'react-native';
+import { useEffect, useState } from 'react';
+import { View, Text, FlatList, Button, Platform, ScrollView, StyleSheet, StatusBar } from 'react-native';
+import ProductCard from '../components/ProductCard';
 
 
-export default function Products ({navigation,route})  {
+export default function Products({ navigation, route }) {
   const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    fetch('https://app.signalgas.io/api/v1/products', {
-      headers: {
-        Authorization: "Bearer "+route.params.token,
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('Fetched products:', data); 
-        
-        
-        setProducts(data);
+  
+
+    useEffect(() => {
+      fetch('https://app.signalgas.io/api/v1/products', {
+        headers: {
+          Authorization: "Bearer " + route.params.token,
+        },
       })
-      .catch((error) => console.error(error));
-  }, []);
+        .then((response) => response.json())
+        .then((data) => {
+          console.log('Fetched products:', data);
+
+
+          setProducts(data);
+        })
+        .catch((error) => console.error(error));
+    }, []);
 
   return (
-    <View>
-     
-      <Button title="Countries" onPress={()=>navigation.navigate("Countries",{token:route.params.token})}/>
-      <Text>All Products</Text>
+    <View style={styles.scrollView}>
       <FlatList
         data={products}
+        renderItem={({ item }) => {
+          console.log(item.id);
+         
+
+
+          return (
+            ProductData = {
+              name: item.name,
+              image: item.name==="3KG Tank"?require("../assets/gasimages/3kg.png"):item.name==="5KG Tank"?require("../assets/gasimages/5kg.jpeg"):item.name==="7KG Tank"?require("../assets/gasimages/7kg.jpeg"):require("../assets/gasimages/3kg.png"),
+              type: item.price,
+              hp: item.weight,
+              created_at: item.created_at,
+              updated_at: item.updated_at,
+            },
+
+            <ProductCard {...ProductData} />
+          );
+        }}
         keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <View>
-            <Text>{item.name}</Text>
-            <Text>{item.description}</Text>
-            <Text>{item.price}</Text>
-            <Text>{item.weight}</Text>
-            <Text>{item.country_id}</Text>
-            <Text>{item.created_at}</Text>
-            <Text>{item.updated_at}</Text>
-          </View>
-        )}
+        ItemSeparatorComponent={
+          <View
+            style={{
+              height: 16,
+            }}
+          />
+        }
+        ListEmptyComponent={<Text>No Items Found</Text>}
+        ListHeaderComponent={
+          <Text style={styles.headerText}>Products List</Text>
+        }
+        ListFooterComponent={
+          <Text style={styles.footerText}>End of list</Text>
+        }
+      // horizontal={true}
       />
     </View>
+
+
+
   );
 };
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5F5F5",
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    paddingHorizontal: 16,
+  },
+  card: {
+    backgroundColor: "#FFFFFF",
+    padding: 16,
+    borderRadius: 8,
+    // marginBottom: 16,
+    borderWidth: 1,
+  },
+  cardText: {
+    fontSize: 30,
+  },
+  headerText: {
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 12,
+  },
+  footerText: {
+    fontSize: 24,
+    textAlign: "center",
+    marginTop: 12,
+  },
+  sectionHeaderText: {
+    backgroundColor: "#FFFFFF",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
 
